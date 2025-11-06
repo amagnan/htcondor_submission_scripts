84d92f7cd2d3d5be2bb122c802c3030fc84f49a7
Anne-Marie Magnan <a.magnan@imperial.ac.uk>
2025-11-06 14:09:47 +0100

    adding also loop over x offset

--- fixedTarget/batch/gangaScript.py
+++ fixedTarget/batch/gangaScript.py
@@ -3,24 +3,30 @@ import os
 import random
 random.seed(os.environ.get("USER"))
 startRun = 0 # int(time.time()) + random.randint(0,10000)
-evtsPerJob = 10 #100000
-evtsToGen = 20
+evtsPerJob = 100000
+evtsToGen = 10000000
 nSJ = int(evtsToGen/evtsPerJob)
 
 #beam parameters
 smear = 0
 paint = 0
-xoff = int(50)
+xoff = 50
+sigma = 16
 ecut = 10
-savestring = f'smear{smear}_paint{paint}_xoffset{xoff}'
 
-j = Job(name = f'run_fixedTarget_{savestring}-{evtsToGen}events')
-j.application = Executable(exe = File('bashScript.sh'),\
-                           args = [savestring, '-o', '"./"', '-n', evtsPerJob,'--no-debug --no-force -e', ecut, '--beam-smear', smear, '--beam-paint', paint, '--x-offset', xoff])
-j.splitter = ArgSplitter(args = [['-r', startRun + _i] for _i in range(nSJ)], append = True)
-j.outputfiles = [] #LocalFile('*.root')]
-j.backend = Condor()
-j.backend.cdf_options['+MaxRuntime'] = '50000'
-j.backend.cdf_options['accounting_group'] = 'group_u_SHIP.u_ship_cg'
-j.comment = f'{evtsPerJob} events in each of {nSJ} subjobs'
-j.submit()
+for nS in range(11):
+#for nS in range(2):
+    myxoff = xoff+nS*sigma
+    savestring = f'smear{smear}_paint{paint}_xoffset{myxoff}'
+    print(savestring)
+
+    j = Job(name = f'run_fixedTarget_{savestring}-{evtsToGen}events')
+    j.application = Executable(exe = File('bashScript.sh'),\
+                               args = [savestring, '-o', '"./"', '-n', evtsPerJob,'--no-debug --no-force -e', ecut, '--beam-smear', smear, '--beam-paint', paint, '--x-offset', myxoff])
+    j.splitter = ArgSplitter(args = [['-r', startRun + _i] for _i in range(nSJ)], append = True)
+    j.outputfiles = [] #LocalFile('*.root')]
+    j.backend = Condor()
+    j.backend.cdf_options['+MaxRuntime'] = '50000'
+    j.backend.cdf_options['accounting_group'] = 'group_u_SHIP.u_ship_cg'
+    j.comment = f'{evtsPerJob} events in each of {nSJ} subjobs'
+    j.submit()
