64d66e28b0b7a7126eb40d4cde333afb2e1cc265
Anne-Marie Magnan <a.magnan@imperial.ac.uk>
2025-11-06 12:51:27 +0100

    fix copy to eos

--- fixedTarget/batch/bashScript.sh
+++ fixedTarget/batch/bashScript.sh
@@ -3,10 +3,17 @@
 LOCALDIR=`pwd`
 FS_INSTALL=/afs/cern.ch/user/a/ammagnan
 
+#access string to save output files
+savestring=$1
+
+# Remove the first argument
+shift
+
 export EOS_MGM_URL=root://eospublic.cern.ch
-EOSDIR=/eos/experiment/ship/user/ammagnan/TargetProd/251106 
+EOSDIR=/eos/experiment/ship/user/ammagnan/TargetProd/251106/$savestring 
 mkdir -p $EOSDIR
 
+echo "INFO: eos dir set to "$EOSDIR
 source /cvmfs/ship.cern.ch/25.09/setUp.sh
 
 export ALIBUILD_WORK_DIR=${FS_INSTALL}/sw
@@ -22,7 +29,22 @@ python ${FS_INSTALL}/FairShip/muonShieldOptimization/run_fixedTarget.py $@
 echo "INFO: Finished running. These files are on the WN:"
 ls -lh *
 
-rootdir=`ls -d */ | grep fixedTarget`
-cp $rootdir/geofile_full.root $EOSDIR/${rootdir}_geofile_full.root
-file=`find $rootdir/. -type f -exec basename {} \; | grep pythia8`
-cp $rootdir/$file.root $EOSDIR/${rootdir}_${file}.root
+#cp geofile_full.root $EOSDIR/geofile_full.root
+myfile=`ls pythia*.root`
+cp $myfile $EOSDIR/.
+
+if (( "$?" != "0" )); then
+    echo " --- Problem with copy of ${myfile} file to EOS. Keeping locally."
+else
+    eossize=`ls -l $EOSDIR/$myfile | awk '{print $5}'`
+    localsize=`ls -l ${myfile} | awk '{print $5}'`
+    if [ $eossize != $localsize ]; then
+        echo " --- Copy of ${myfile} file to eos failed. Localsize = $localsize, eossize = $eossize. Keeping locally..."
+    else
+        echo " --- Size check done: Localsize = $localsize, eossize = $eossize"
+	rm *.root
+    fi
+fi
+
+echo "INFO: Files copied to eos!"
+ls -ltrh $EOSDIR/*
--- fixedTarget/batch/gangaScript.py
+++ fixedTarget/batch/gangaScript.py
@@ -4,7 +4,7 @@ import random
 random.seed(os.environ.get("USER"))
 startRun = 0 # int(time.time()) + random.randint(0,10000)
 evtsPerJob = 10 #100000
-evtsToGen = 100
+evtsToGen = 20
 nSJ = int(evtsToGen/evtsPerJob)
 
 #beam parameters
@@ -12,12 +12,13 @@ smear = 0
 paint = 0
 xoff = 50
 ecut = 10
+savestring = f'smear{smear}_paint{paint}_xoffset{xoff}'
 
-j = Job(name = f'run_fixedTarget_smear{smear}_paint{paint}_xoffset{xoff}-{evtsToGen}events')
+j = Job(name = f'run_fixedTarget_{savestring}-{evtsToGen}events')
 j.application = Executable(exe = File('bashScript.sh'),\
-                           args = ['-o', '"./"', '-n', evtsPerJob,'--no-debug -e', ecut, '--beam-smear=', smear, '--beam-paint=', paint, '--x-offset=', xoff])
+                           args = [savestring, '-o', '"./"', '-n', evtsPerJob,'--no-debug -e', ecut, '--beam-smear=', smear, '--beam-paint=', paint, '--x-offset=', xoff])
 j.splitter = ArgSplitter(args = [['-r', startRun + _i] for _i in range(nSJ)], append = True)
-j.outputfiles = [LocalFile('*.root')]
+j.outputfiles = [] #LocalFile('*.root')]
 j.backend = Condor()
 j.backend.cdf_options['+MaxRuntime'] = '50000'
 j.backend.cdf_options['accounting_group'] = 'group_u_SHIP.u_ship_cg'
